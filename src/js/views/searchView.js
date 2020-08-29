811fd3c82949c3609c099fd99e6bf7325936904c
Ajay Raj <ajayraj003016@gmail.com>
2020-08-29 14:18:50 +0530

    set the limits in title

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,14 +10,29 @@ export const clearResult = () => {
     elements.searchResList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 15) => {
+    const newTitle = [];
+    if(title.length > limit) {
+        title.split(' ').reduce((acc, curr) => {
+            if(acc + curr.length <= limit) {
+                newTitle.push(curr);
+            }
+            return acc + curr.length;
+        }, 0);
+
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+}
+
 const renderRecipes = recipe => {
     const markup = `<li>
-                    <a class="results__link results__link--active" href="#${recipe.recipe_id}">
+                    <a class="results__link" href="#${recipe.recipe_id}">
                         <figure class="results__fig">
                             <img src=${recipe.image_url} alt=${recipe.title}>
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
