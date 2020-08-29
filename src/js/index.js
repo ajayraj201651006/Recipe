ef1765a397ccfa0863a13d347d0b7e573a98b7be
Ajay Raj <ajayraj003016@gmail.com>
2020-08-29 13:20:18 +0530

    implement search functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search'; //importing the search class
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 /****
 * Global State of the App
@@ -12,23 +14,27 @@ const state = {};
 //control search function-->
 const controlSearch = async () => {
     // 1. get query from the view
-    const query = 'pizza';
+    const query = searchView.getInput();
+    console.log(query);
     
     if(query) {
         // 2. create search object in the state
         state.search = new Search(query);
         
         // 3. prepare the UI for results
+        searchView.clearInput();
+        searchView.clearResult();
 
         // 4. get results for search
         await state.search.getRecipes();
 
         // 5. Renders results on the UI
         console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
