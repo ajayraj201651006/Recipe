8471eec77d9820e41391e64d2f86457307e5b93c
Ajay Raj <ajayraj003016@gmail.com>
2020-08-30 15:13:32 +0530

    builds like model

---
+++ src/js/models/Likes.js
@@ -0,0 +1,27 @@
+export default class Likes {
+    constructor() {
+        this.likes = [];
+    }
+
+    addLike(id, title, author, img) {
+        const like = {id, title, author, img};
+        this.likes.push(like);
+        return like;
+    }
+
+    deleteLike(id) {
+        //find the index of the delete like
+        const findLikeIndex = this.likes.findIndex(el => el.id === id);
+
+        //remove the like
+        this.likes.splice(findLikeIndex, 1);
+    }
+
+    isLiked(id) {
+        return this.likes.findIndex(el => el.id === id) !== -1;
+    }
+
+    getNumLikes() {
+        return this.likes.length;
+    }
+}
